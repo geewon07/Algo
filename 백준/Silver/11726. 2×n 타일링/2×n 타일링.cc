575faa045db42e52ffbdc430ea493fcb1b9e6422
jiwon07 <122420135+geewon07@users.noreply.github.com>
2023-04-10 11:44:51 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define MAX_N 1005
+using namespace std;
+long dp[MAX_N];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int n; cin >> n;
+	dp[0] = 0; dp[1] = 1; dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+		if (dp[i] > 10007)dp[i] %= 10007;
+	}
+	cout << dp[n];
+	return 0;
+}
