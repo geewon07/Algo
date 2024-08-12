285b37cdc1aca70170c7ae5f5e350992ba65d81a
jiwon07 <122420135+geewon07@users.noreply.github.com>
2024-08-12 19:05:36 +0900

    [Bronze III] Title: 통학의 신, Time: 68 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,21 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st  = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        for(int i = -1000 ; i <= 1000; i++) {
-            if(Math.pow(i,2) + 2 * a * i + b == 0 ) {
+        for(int i=-1000;i<=1000;i++) {
+            if(Math.pow(i,2) + 2*a*i + b == 0)  {
                 System.out.print(i+" ");
             }
         }
