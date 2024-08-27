93fb01350de7d1bd7b3ae75ebeab23feae3c40da
jiwon07 <122420135+geewon07@users.noreply.github.com>
2024-08-27 23:10:57 +0900

    [Silver V] Title: 색종이, Time: 64 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,29 +1,40 @@
-import java.util.Scanner;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	public static void main(String[] args){
-		Scanner  sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[][] arr = new int[100][100];
-		
-		int count= 0;
-		for(int i=0;i<N;i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			
-			for(int a=x;a<x+10;a++) {
-				for(int b=y;b<y+10;b++) {
-					arr[a][b] = 1;
-				}
-			}
-		}
-		
-		for(int i=0;i<100;i++) {
-			for(int j=0;j<100;j++) {
-				if(arr[i][j] == 1) count++;
-			}
-		}
-		System.out.println(count);
-}
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        int[][] board = new int[100][100];
+
+        int answer = 0;
+
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int a = x; a < x + 10; a++) {
+                for (int b = y; b < y + 10; b++) {
+                    board[a][b] = 1;
+                }
+            }
+        }
+
+            for(int i=0;i<100;i++) {
+                for(int j=0;j<100;j++) {
+                    if(board[i][j] == 1) answer++;
+                }
+            }
+
+
+
+
+
+        System.out.println(answer);
+    }
 }
