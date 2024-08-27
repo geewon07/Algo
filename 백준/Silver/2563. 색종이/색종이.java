c41c31a876983f5e204b18a9d2c9e6a91591baec
jiwon07 <122420135+geewon07@users.noreply.github.com>
2024-08-27 23:06:53 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 13068 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner  sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[][] arr = new int[100][100];
+		
+		int count= 0;
+		for(int i=0;i<N;i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			for(int a=x;a<x+10;a++) {
+				for(int b=y;b<y+10;b++) {
+					arr[a][b] = 1;
+				}
+			}
+		}
+		
+		for(int i=0;i<100;i++) {
+			for(int j=0;j<100;j++) {
+				if(arr[i][j] == 1) count++;
+			}
+		}
+		System.out.println(count);
+}
+}
