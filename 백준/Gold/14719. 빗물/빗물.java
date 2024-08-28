33671de4a637d67fbc3755dfb15ecdbe654b030e
jiwon07 <122420135+geewon07@users.noreply.github.com>
2024-08-28 20:25:01 +0900

    [Gold V] Title: 빗물, Time: 68 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main { // 누적합으로 풀었을 때
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,26 +11,24 @@ public class Main {
 
         int[] harr = new int[w];
         int answer = 0;
+        int[] prefix_max = new int[w+2];
+        int[] suffix_max = new int[w+2];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<w;i++) {
             harr[i] = Integer.parseInt(st.nextToken());
+            prefix_max[i+1] = Math.max(prefix_max[i], harr[i]);
+        }
+        for(int i=w-1;i>=0;i--) {
+            suffix_max[i+1] = Math.max(suffix_max[i+2],harr[i]);
         }
 
-        for(int i=h;i>=1;i--) {
-            int count = 0;
-            boolean check = false;
-            for(int j=0;j<w;j++) {
-                if(harr[j] >= i) {
-                    check = true;
-                    answer += count;
-                    count = 0;
-                }else {
-                    if(check) count++;
-                }
-//                System.out.println(i+" :높이 " + j+" :지금 위치 "+count+" :count값 "+answer+" answer 값 "+check+"harr[j]의 값: "+harr[j]);
-            }
+        for(int i=0;i<w;i++) {
+            int curr = Math.min(prefix_max[i+1],suffix_max[i+1]);
+            if(harr[i] < curr ) answer += curr - harr[i];
         }
+
+
         System.out.println(answer);
     }
 }
