5fbbc84d4d03c9a5bfbc51de3a798d749686321f
jiwon07 <122420135+geewon07@users.noreply.github.com>
2024-10-30 15:05:24 +0900

    [Silver II] Title: 부분수열의 합, Time: 76 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,89 +1,44 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
+    static int N,S;
+    static int[] arr;
+    static int answer = 0;
 
-    static void input() {
-        N = scan.nextInt();
-        S = scan.nextInt();
-        nums = new int[N + 1];
-        for (int i = 1; i <= N; i++) nums[i] = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        arr = new int[N];
+
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        recur(0,0);
+
+        if(S==0) answer--; // S = 0일 때 공집합 카운트 됨!
+        System.out.println(answer);
     }
 
-    static int N, S, ans;
-    static int[] nums;
-    // k번째 원소를 포함시킬 지 정하는 함수
-    // value:= k-1 번째 원소까지 골라진 원소들의 합
-    static void rec_func(int k, int value) {
-        if (k == N + 1) {  // 부분 수열을 하나 완성 시킨 상태
-            // value 가 S 랑 같은 지 확인하기
-            if (value == S) ans++;
-        } else {
-            // k 번째 원소를 포함시킬 지 결정하고 재귀호출해주기
-            // Include
-            rec_func(k + 1, value + nums[k]);
-            // Not Include
-            rec_func(k + 1, value);
-        }
-    }
-
-    public static void main(String[] args) {
-        input();
-        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
-        rec_func(1, 0);
-        // ans 가 정말 "진 부분집합"만 다루는 지 확인하기
-        if (S == 0){
-            ans--;
-        }
-        System.out.println(ans);
-    }
-
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    private static void recur(int count, int sum) {
+        if(count == N) {
+            if (sum == S) {
+                answer++;
             }
-            return st.nextToken();
+            return;
         }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
+        recur(count+1, sum + arr[count]);
+        recur(count+1, sum);
     }
+
+
 }
