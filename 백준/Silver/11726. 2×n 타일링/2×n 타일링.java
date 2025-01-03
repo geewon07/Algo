44ba98a7c864cb636096ece7f5a83b5f9b8b9fd1
jiwon07 <122420135+geewon07@users.noreply.github.com>
2025-01-03 22:56:19 +0900

    [Silver III] Title: 2×n 타일링, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int X = Integer.parseInt(br.readLine());
+    	
+    	int[] dp = new int[1001];
+    	dp[1] = 1;
+    	dp[2] = 2;
+    	
+    	for(int i=3;i<X+1;i++) {
+    		dp[i] = (dp[i-1] + dp[i-2])%10007;
+    	}
+    	System.out.println(dp[X]);
+    		}
+    }
